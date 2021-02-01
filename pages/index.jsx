6e97952eafb62b00d596580ae8e25803209d1253
Redha Azmei <redha.azmei@gmail.com>
2021-02-01 13:14:40 +0800

    Add favicon and update homepage component

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import { Heading } from "@chakra-ui/react";
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Redha Azmei - Developer</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+        <link rel="manifest" href="favicon/site.webmanifest" />
+      </Head>
+      <Heading>Welcome to my blog!</Heading>
+    </>
+  );
+};
+
+export default Home;
