b35cb4b272e7eecf23982e9f442af711234a85a0
Redha Azmei <redha.azmei@gmail.com>
2021-02-05 11:33:42 +0800

    Add 3xl maxW container

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,16 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Box bgGradient="linear(to-r, #396afc, #2948ff)" h="2" />
       <Flex justify="center">
-        <Box maxW="5xl" w="100%" mt="5">
+        <Box maxW="5xl" w="100%" mt="5" mx="8">
           <Navbar />
+          <Box maxW="3xl" w="100%" mx="auto">
+            {children}
+          </Box>
         </Box>
       </Flex>
     </>
