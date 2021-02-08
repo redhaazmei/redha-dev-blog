10ad2d9fc64065f2c7c6058000b548f59e5144c0
Redha Azmei <redha.azmei@gmail.com>
2021-02-09 02:38:30 +0800

    Update Author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,12 +1,14 @@
-import { Heading, Text, Link } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const Author = () => {
   return (
     <>
-      <Heading as="h1" fontSize={["2xl", "5xl"]} mb="2">
-        Hey, I'm Redha Azmei
-      </Heading>
-      <Text>I am a Data Scientist with deep interest in ML, AI and modern Web Technologies. I am currently working in the aviation industry based in Malaysia 🇲🇾. I enjoy learning new stuffs by building small scale projects.</Text>
+      <Box mb="16">
+        <Heading as="h1" fontSize={["2xl", "5xl"]} mb="2">
+          Hey, I'm Redha Azmei
+        </Heading>
+        <Text>I am a Data Scientist with deep interest in ML, AI and modern Web Technologies. I am currently working in the aviation industry based in Malaysia 🇲🇾. I enjoy learning new stuffs by building small scale projects.</Text>
+      </Box>
     </>
   );
 };
