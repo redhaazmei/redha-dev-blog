617f31d470acdd3194e73aa384691a9f263e6492
Redha Azmei <redha.azmei@gmail.com>
2021-02-09 02:37:53 +0800

    Add data files

---
+++ data/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    title: "Maxis IoT Challenge",
+    description: "Hackathon organized by Maxis to test out NB-IoT network.",
+    year: "2019",
+    website: "https://google.com",
+    github: "https://github.com/redhaazmei",
+  },
+  {
+    title: "MyHackathon - Kuching",
+    description: "Hackathon organized by Maxis to test out NB-IoT network.",
+    year: "2020",
+    website: "https://google.com",
+    github: "https://github.com/redhaazmei",
+  },
+  {
+    title: "Teraju Superb",
+    description: "Flood AI Solution to tackle Klang Valley flooding issue",
+    year: "2019",
+    website: "https://google.com",
+    github: "https://github.com/redhaazmei",
+  },
+  {
+    title: "GeoHack",
+    description: "Flood AI Solution to tackle Klang Valley flooding issue",
+    year: "2019",
+    website: "https://google.com",
+    github: "https://github.com/redhaazmei",
+  },
+];
+
+export default projects;
