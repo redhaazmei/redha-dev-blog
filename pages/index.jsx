5b7b5870b723c92a554850b2a920a9de5aecc4b0
Redha Azmei <redha.azmei@gmail.com>
2021-02-10 03:41:24 +0800

    Update index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Layout from "components/Layout";
 import Author from "components/Author";
 import Project from "components/Project";
 import CustomHead from "components/CustomHead";
+import Blog from "components/Blog";
 
 const Home = () => {
   return (
@@ -10,7 +11,7 @@ const Home = () => {
       <Layout>
         <Author />
         <Project />
-        <Project />
+        <Blog />
       </Layout>
     </>
   );
