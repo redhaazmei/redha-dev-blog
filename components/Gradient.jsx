c65f3fd929768a3dd71312ab5fffe21475719c81
Redha Azmei <redha.azmei@gmail.com>
2021-02-09 22:29:44 +0800

    Add Gradient component

---
+++ components/Gradient.jsx
@@ -0,0 +1,4 @@
+const gradientRight = "linear(to-r, brand.lightblue, brand.darkblue)";
+const gradientLeft = "linear(to-l, brand.lightblue, brand.darkblue)";
+
+export { gradientRight, gradientLeft };
