047cafe411688c0c599a4edeb151c5d14309837b
Redha Azmei <redha.azmei@gmail.com>
2021-02-10 03:38:59 +0800

    Add blogs data

---
+++ data/blogs.js
@@ -0,0 +1,25 @@
+const blogs = [
+  {
+    id: 1,
+    title: "Blog Post 1",
+    slug: "blog-post-1",
+    description: "This is an example of description of blog post number 1",
+    date: "8 February 2021",
+  },
+  {
+    id: 2,
+    title: "Blog Post 2",
+    slug: "blog-post-2",
+    description: "This is an example of description of blog post number 2",
+    date: "10 February 2021",
+  },
+  {
+    id: 3,
+    title: "Blog Post 3",
+    slug: "blog-post-3",
+    description: "This is an example of description of blog post number 3",
+    date: "11 February 2021",
+  },
+];
+
+export default blogs;
