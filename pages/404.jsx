db11580c03c27f0bc8622e162890ae0a522c80eb
Redha Azmei <redha.azmei@gmail.com>
2021-02-13 03:20:40 +0800

    Update 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
-import Layout from "components/Layout";
 import { gradientLeft, gradientRight } from "components/Gradient";
-import { Box, Button, Flex, Heading, Text, useColorModeValue } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text, useColorModeValue } from "@chakra-ui/react";
 import { FiHome } from "react-icons/fi";
 import CustomHead from "components/CustomHead";
 
@@ -9,24 +8,22 @@ const Home = () => {
   return (
     <>
       <CustomHead title="404 | Redha Azmei" />
-      <Layout>
-        <Flex justify="center" mb="4">
-          <Flex textAlign="center" maxW="2xl" direction="column" align="center" justify="center">
-            <Heading as="h1" size="4xl">
-              404 Error
-            </Heading>
-            <Heading as="h1" fontSize={["2xl", "5xl"]} pb="4">
-              Page Not Found
-            </Heading>
-            <Text pb="4">It seems you've found something that used to exist, or you spelled something wrong. I'm guessing you spelled something wrong. Can you double check that URL?</Text>
-            <Link href="/">
-              <Button _hover={{ color: "blue.900" }} color="white" bgGradient={useColorModeValue(gradientLeft, gradientRight)} leftIcon={<FiHome />}>
-                Back to Home
-              </Button>
-            </Link>
-          </Flex>
+      <Flex justify="center" mb="4">
+        <Flex textAlign="center" maxW="2xl" direction="column" align="center" justify="center">
+          <Heading as="h1" size="4xl">
+            404 Error
+          </Heading>
+          <Heading as="h1" fontSize={["2xl", "5xl"]} pb="4">
+            Page Not Found
+          </Heading>
+          <Text pb="4">It seems you've found something that used to exist, or you spelled something wrong. I'm guessing you spelled something wrong. Can you double check that URL?</Text>
+          <Link href="/">
+            <Button _hover={{ color: "blue.900" }} color="white" bgGradient={useColorModeValue(gradientLeft, gradientRight)} leftIcon={<FiHome />}>
+              Back to Home
+            </Button>
+          </Link>
         </Flex>
-      </Layout>
+      </Flex>
     </>
   );
 };
