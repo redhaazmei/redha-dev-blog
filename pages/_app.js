e6421b0f8ec708866a14e9315150d3a54fd0e32e
Redha Azmei <redha.azmei@gmail.com>
2021-02-13 03:21:22 +0800

    insert Layout component to suit whole site layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "focus-visible/dist/focus-visible";
 import theme from "../theme";
 import CustomFonts from "../fonts";
+import Layout from "components/Layout";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <ChakraProvider theme={theme}>
         <CustomFonts />
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ChakraProvider>
     </>
   );
