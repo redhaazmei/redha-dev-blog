e1623ea3e0c6039064f72d4d595955cac63c3551
Redha Azmei <redha.azmei@gmail.com>
2021-02-15 03:59:37 +0800

    Update Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
@@ -7,7 +7,6 @@ const Layout = ({ children }) => {
     <>
       <Box bgGradient="linear(to-r, brand.red, brand.orange)" h="2" />
       <Flex justify="center">
-        {/* <Flex bg={useColorModeValue("white", "brand.darkpurple")} justify="center"> */}
         <Box maxW="5xl" w="100%" mt="5" mx="8">
           <Navbar />
           <Box maxW="4xl" w="100%" mx="auto">
