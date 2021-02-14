55c34ec622d58dfe29b87e5c6a8e279e8dc41d88
Redha Azmei <redha.azmei@gmail.com>
2021-02-15 03:58:31 +0800

    Remove unused fonts

--- fonts.js
+++ fonts.js
@@ -2,38 +2,17 @@ import { Global } from "@emotion/react";
 
 const fontlist = `
   @font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: url(/fonts/Inter-Regular.ttf);
-  }
-  @font-face {
-    font-family: 'Inter';
-    font-style: bold;
-    font-weight: 700;
-    font-display: swap;
-    src: url(/fonts/Inter-Bold.ttf);
-  }
-  @font-face {
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: url(/fonts/OpenSans-Regular.ttf);
-  }
-  @font-face {
     font-family: 'DM Sans';
     font-style: normal;
     font-weight: 400;
-    font-display: swap;
+    font-display: optional;
     src: url(/fonts/DMSans-Regular.ttf);
   }
   @font-face {
     font-family: 'DM Sans';
     font-style: bold;
     font-weight: 700;
-    font-display: swap;
+    font-display: optional;
     src: url(/fonts/DMSans-Bold.ttf);
   }
 `;
