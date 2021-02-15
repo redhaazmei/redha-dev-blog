e40d15c9280c4e446a47196e1302974b5f35bf06
Redha Azmei <redha.azmei@gmail.com>
2021-02-15 16:44:55 +0800

    Update colorscheme

--- components/CardBlog.jsx
+++ components/CardBlog.jsx
@@ -8,7 +8,7 @@ const CardBlog = () => {
       <SimpleGrid columns={[1]} spacing="4">
         {blogs.map((blog) => {
           return (
-            <LinkBox key={blog.id} minH="2em" w="100%" bg={[useColorModeValue("gray.200", "gray.900"), "inherit"]} _hover={{ bg: useColorModeValue("gray.200", "gray.900"), cursor: "pointer" }} p="4" role="group">
+            <LinkBox key={blog.id} minH="2em" w="100%" _hover={{ bg: useColorModeValue("gray.50", "brand.purple700"), cursor: "pointer" }} p="4" role="group">
               <Flex direction={["column", "row"]} justify="space-between">
                 <Box pb="2">
                   <Link href={`/blog/${blog.slug}`}>
