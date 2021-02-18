2252fb1961b959e652f1ca16fe8a3b7074c3c24d
Redha Azmei <redha.azmei@gmail.com>
2021-02-18 10:26:30 +0800

    Update MDX components

--- components/MDXComponents.jsx
+++ components/MDXComponents.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import { Button } from "@chakra-ui/react";
+import { Button, Code } from "@chakra-ui/react";
 
 const MDXComponents = {
   Image,
   Button,
+  Code,
 };
 
 export default MDXComponents;
