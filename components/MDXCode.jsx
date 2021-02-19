6a51b5cc6c22755cdf18fc6ac3ab4222cfc966e2
Redha Azmei <redha.azmei@gmail.com>
2021-02-20 03:32:44 +0800

    Modify MDXCode syntax highlighting styling

--- components/MDXCode.jsx
+++ components/MDXCode.jsx
@@ -5,7 +5,7 @@ import nightOwl from "prism-react-renderer/themes/nightOwl";
 const CopyButton = ({ value }) => {
   const { onCopy, hasCopied } = useClipboard(value);
   return (
-    <Button variant="solid" bg="brand.red" w="3.5rem" size="xs" borderRadius="md" color="white" _hover={{ bg: "brand.red100" }} onClick={onCopy}>
+    <Button variant="solid" bg="brand.red" w="3.5rem" size="xs" borderRadius="md" color="white" mt="3" _hover={{ bg: "brand.red100" }} onClick={onCopy}>
       {hasCopied ? "COPIED" : "COPY"}
     </Button>
   );
@@ -21,7 +21,7 @@ const MDXCode = ({ children, className }) => {
       </Flex>
       <Highlight {...defaultProps} theme={nightOwl} code={children} language={language}>
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={className} style={{ ...style, fontSize: "0.8rem", background: "none" }}>
+          <pre className={className} style={{ ...style, fontSize: "0.8rem", background: "none", marginBottom: 0, padding: 0 }}>
             {tokens.map((line, i) => (
               <div key={i} {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (
