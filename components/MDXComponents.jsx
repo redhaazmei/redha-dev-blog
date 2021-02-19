90b1d7ec427a22ec787c6784edbb9d05ecdadda4
Redha Azmei <redha.azmei@gmail.com>
2021-02-20 03:33:10 +0800

    Update MDXComponents with MDXCode and MDXImage

--- components/MDXComponents.jsx
+++ components/MDXComponents.jsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
-import { Button, Code } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import MDXCode from "components/MDXCode";
+import MDXImage from "components/MDXImage";
+import MDXBlockquote from "components/MDXBlockquote";
 
 const MDXComponents = {
+  MDXImage,
   Image,
-  code: MDXCode,
   Button,
-  Code,
+  code: MDXCode,
+  blockquote: MDXBlockquote,
 };
 
 export default MDXComponents;
