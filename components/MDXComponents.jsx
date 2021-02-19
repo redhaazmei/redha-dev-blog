fb62c9d296fb44a08ab544f29f199c12fed0d960
Redha Azmei <redha.azmei@gmail.com>
2021-02-19 17:41:29 +0800

    Update MDXComponenets with MDXCode codeblock for syntax highlighting

--- components/MDXComponents.jsx
+++ components/MDXComponents.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { Button, Code } from "@chakra-ui/react";
+import MDXCode from "components/MDXCode";
 
 const MDXComponents = {
   Image,
+  code: MDXCode,
   Button,
   Code,
 };
