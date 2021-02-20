6c3d67ea9442c556fa7f32dd8dea98d832355da2
Redha Azmei <redha.azmei@gmail.com>
2021-02-21 04:56:51 +0800

    Modify BlogTitle styling and add reading-time

--- components/BlogTitle.jsx
+++ components/BlogTitle.jsx
@@ -1,14 +1,21 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Badge, Box, Stack, Heading, Text } from "@chakra-ui/react";
 
-const BlogTitle = ({ title, date, readTime }) => {
+const BlogTitle = ({ title, date, readtime, category }) => {
   return (
     <>
-      <Box mb="16">
-        <Heading fontSize={["4xl", "6xl"]}>{title}</Heading>
-        <Flex>
-          <Text>{date}</Text>
-          <Text>{readTime}</Text>
-        </Flex>
+      <Box mb="12">
+        <Heading fontSize={["4xl", "6xl"]} mb="3">
+          {title}
+        </Heading>
+        <Stack spacing={["auto", "4"]} direction={["column", "row"]}>
+          <Text>🗓️ {date}</Text>
+          <Text d={["none", "block"]}>|</Text>
+          <Text>⏱️ {readtime}</Text>
+          <Text d={["none", "block"]}>|</Text>
+          <Text alignItems="center">
+            <Badge colorScheme={category === "Web Development" ? "blue" : category === "Data Science" ? "green" : category === "ML/AI" ? "purple" : "red"}>{category}</Badge>
+          </Text>
+        </Stack>
       </Box>
     </>
   );
