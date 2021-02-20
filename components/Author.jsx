8b02a34a046280a905687551fe3910b8f83b511a
Redha Azmei <redha.azmei@gmail.com>
2021-02-21 00:48:37 +0800

    Modify Heading text size at smaller breakpoints to 2xl

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,7 +4,7 @@ const Author = () => {
   return (
     <>
       <Box mb="16">
-        <Heading as="h1" fontSize={["3xl", "5xl"]} mb="2">
+        <Heading as="h1" fontSize={["2xl", "5xl"]} mb="2">
           Hi, I'm Redha Azmei 👋
         </Heading>
         <Text>I am a Data Scientist with deep interest in ML, AI and modern Web Technologies. I am currently working in the aviation industry based in Malaysia 🇲🇾. Learning new techs by building small scale projects.</Text>
