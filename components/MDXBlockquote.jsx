361309ebfd966ae1eaf707514d43a52662d6e98d
Redha Azmei <redha.azmei@gmail.com>
2021-02-21 01:48:31 +0800

    Modify MDX Blockquote styling

--- components/MDXBlockquote.jsx
+++ components/MDXBlockquote.jsx
@@ -1,8 +1,8 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 
 const MDXBlockquote = ({ children }) => {
   return (
-    <Box as="blockquote" borderLeft="solid" borderColor="brand.red" pl="6" fontWeight="bold" fontStyle="italic">
+    <Box as="blockquote" bg={useColorModeValue("gray.50", "brand.purple700")} borderLeft="solid" borderColor="brand.red" pl="6" pr="4" py="4" mb="8" fontWeight="bold">
       {children}
     </Box>
   );
