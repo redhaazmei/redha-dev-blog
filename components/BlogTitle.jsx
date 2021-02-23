b66cdfb1b23594d125a251c5c6f02ba3dff3a8d5
Redha Azmei <redha.azmei@gmail.com>
2021-02-23 22:45:48 +0800

    Add Next Image component to be displayed below blog title

--- components/BlogTitle.jsx
+++ components/BlogTitle.jsx
@@ -1,6 +1,7 @@
+import Image from "next/image";
 import { Badge, Box, Stack, Heading, Text } from "@chakra-ui/react";
 
-const BlogTitle = ({ title, date, readtime, category }) => {
+const BlogTitle = ({ title, date, readtime, category, image }) => {
   return (
     <>
       <Box mb="12">
@@ -16,6 +17,9 @@ const BlogTitle = ({ title, date, readtime, category }) => {
             <Badge colorScheme={category === "Web Development" ? "blue" : category === "Data Science" ? "green" : category === "ML/AI" ? "purple" : "red"}>{category}</Badge>
           </Text>
         </Stack>
+        <Box mt="4">
+          <Image alt={title} src={image} width={1200} height={630} />
+        </Box>
       </Box>
     </>
   );
