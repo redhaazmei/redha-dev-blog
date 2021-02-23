a80639586a6c2a3f8e533c788e6794bf6bbf95b3
Redha Azmei <redha.azmei@gmail.com>
2021-02-23 23:16:47 +0800

    Rename initial Card component to reserve. Might use later

---
+++ components/reserve.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { LinkBox, LinkOverlay, Box, Flex, Heading, Text, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
+import blogs from "data/blogs";
+
+const CardBlog = () => {
+  return (
+    <>
+      <SimpleGrid columns={[1]} spacing="4">
+        {blogs.map((blog) => {
+          return (
+            <LinkBox key={blog.id} minH="2em" w="100%" _hover={{ bg: useColorModeValue("gray.50", "brand.purple700"), cursor: "pointer" }} p="4" role="group">
+              <Flex direction={["column", "row"]} justify="space-between">
+                <Box pb="2">
+                  <Link href={`/blog/${blog.slug}`}>
+                    <LinkOverlay>
+                      <Heading size="sm" _groupHover={{ color: "brand.red" }}>
+                        {blog.title}
+                      </Heading>
+                      <Text fontSize="sm">{blog.description}</Text>
+                    </LinkOverlay>
+                  </Link>
+                </Box>
+                <Heading fontSize={["xs", "sm"]} mb="auto">
+                  {blog.date}
+                </Heading>
+              </Flex>
+            </LinkBox>
+          );
+        })}
+      </SimpleGrid>
+    </>
+  );
+};
+
+export default CardBlog;
