a8935eca8af8b55aef0e0bc77588d91b086be894
Redha Azmei <redha.azmei@gmail.com>
2021-02-25 17:33:17 +0800

    Remove banner image from blog title

--- components/BlogTitle.jsx
+++ components/BlogTitle.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import { Badge, Box, Stack, Heading, Text } from "@chakra-ui/react";
 
-const BlogTitle = ({ title, date, readtime, category, image }) => {
+const BlogTitle = ({ title, date, readtime, category }) => {
   return (
     <>
-      <Box mb="8">
+      <Box mb="12">
         <Heading fontSize={["4xl", "6xl"]} mb="3">
           {title}
         </Heading>
@@ -17,9 +17,6 @@ const BlogTitle = ({ title, date, readtime, category, image }) => {
             <Badge colorScheme={category === "Web Development" ? "blue" : category === "Data Science" ? "green" : category === "ML/AI" ? "purple" : "red"}>{category}</Badge>
           </Text>
         </Stack>
-        <Box mt="4">
-          <Image alt={title} src={image} width={1200} height={630} />
-        </Box>
       </Box>
     </>
   );
