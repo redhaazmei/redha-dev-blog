5a164084a18fea086ab05a321307971e146f46c2
Redha Azmei <redha.azmei@gmail.com>
2021-02-25 17:44:57 +0800

    Add empty projects page. To be updated later

---
+++ pages/projects.jsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+const Projects = () => {
+  return (
+    <>
+      <Head>
+        <title>Projects | Redha Azmei</title>
+      </Head>
+      <Box mb="12">
+        <Heading as="h1" fontSize={["2xl", "5xl"]} mb="2">
+          Projects
+        </Heading>
+        <Text pb="4">Writing is a great way for me to document and share my thoughts, especially on the techs I learned and projects I built. Use the search below to find your favourite articles.</Text>
+      </Box>
+    </>
+  );
+};
+
+export default Projects;
