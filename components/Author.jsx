1a6138cfd5774193f5a6aebdb45bd1a3c41f944c
Redha Azmei <redha.azmei@gmail.com>
2021-02-25 17:32:46 +0800

    Update components

--- components/Author.jsx
+++ components/Author.jsx
@@ -7,7 +7,7 @@ const Author = () => {
         <Heading as="h1" fontSize={["2xl", "5xl"]} mb="2">
           Hi, I'm Redha Azmei 👋
         </Heading>
-        <Text>I am a Data Scientist with deep interest in ML, AI and modern Web Technologies. I am currently working in the aviation industry based in Malaysia 🇲🇾. Learning new techs by building small scale projects.</Text>
+        <Text>I am a Data Scientist with deep interest in ML, AI and modern Web Technologies. I am currently working in the aviation industry based in Malaysia 🇲🇾. Addicted to learning and building techs.</Text>
       </Box>
     </>
   );
