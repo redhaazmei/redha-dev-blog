81206c59e627af6f8618c04c425e6ae1be4f9c0a
Redha Azmei <redha.azmei@gmail.com>
2021-03-04 18:07:59 +0800

    Modify heading fontSize

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
         <title>Projects | Redha Azmei</title>
       </Head>
       <Box mb="12">
-        <Heading as="h1" fontSize={["2xl", "5xl"]} mb="2">
+        <Heading as="h1" fontSize={["3xl", "5xl"]} mb="2">
           Projects
         </Heading>
         <Text pb="4">Writing is a great way for me to document and share my thoughts, especially on the techs I learned and projects I built. Use the search below to find your favourite articles.</Text>
