8c164fa46435c074e16f43af49617116c2971699
Redha Azmei <redha.azmei@gmail.com>
2021-03-04 18:07:35 +0800

    Modify heading fontSize

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -14,7 +14,7 @@ const Blog = ({ posts }) => {
         <title>Blog | Redha Azmei</title>
       </Head>
       <Box mb="12">
-        <Heading as="h1" fontSize={["2xl", "5xl"]} mb="2">
+        <Heading as="h1" fontSize={["3xl", "5xl"]} mb="2">
           Blog
         </Heading>
         <Text pb="4">Writing is a great way for me to document and share my thoughts, especially on the techs I learned and projects I built. Use the search below to find your favourite articles.</Text>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import { NextSeo } from "next-seo";
 import { getposts } from "lib/getposts";
 import { SimpleGrid } from "@chakra-ui/react";
 import Author from "components/Author";
@@ -10,9 +10,28 @@ const Home = ({ posts }) => {
   const filteredposts = posts.sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date))).slice(0, 2);
   return (
     <>
-      <Head>
-        <title>Redha Azmei</title>
-      </Head>
+      <NextSeo
+        title="Redha Azmei"
+        description="A Data Scientist with deep interest in ML, AI and modern Web Technologies. Currently working in the aviation industry based in Malaysia 🇲🇾. Welcome to my personal space where I share all my thoughts on techs."
+        openGraph={{
+          url: "https://redha.dev",
+          title: "redha.dev | Redha Azmei",
+          description: "A Data Scientist with deep interest in ML, AI and modern Web Technologies. Currently working in the aviation industry based in Malaysia 🇲🇾. Welcome to my personal space where I share all my thoughts on techs.",
+          images: [
+            {
+              url: "https://redha.dev/images/redha.dev-banner.png",
+              width: 1200,
+              height: 630,
+              alt: "redha.dev banner",
+            },
+          ],
+          site_name: "redha.dev | Redha Azmei",
+        }}
+        twitter={{
+          handle: "@redhaazmei",
+          cardType: "summary_large_image",
+        }}
+      />
       <Author />
       <SectionLayout heading="Featured Projects" text="Highlights of my recent involvements">
         <ProjectCard />
