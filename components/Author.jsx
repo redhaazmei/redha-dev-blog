1ae38dd5735fb21546751ea9f2843fcae46db1e8
Redha Azmei <redha.azmei@gmail.com>
2021-03-05 09:38:34 +0800

    Change Author fontSize to 1.7rem for mobile screen

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,7 +4,7 @@ const Author = () => {
   return (
     <>
       <Box mb="16">
-        <Heading as="h1" fontSize={["3xl", "5xl"]} mb="2">
+        <Heading as="h1" fontSize={["1.7rem", "5xl"]} mb="2">
           Hi, I'm Redha Azmei 👋
         </Heading>
         <Text>A Data Scientist with deep interest in ML, AI and modern Web Technologies. Currently working in the aviation industry based in Malaysia 🇲🇾. Welcome to my personal space where I share all my thoughts on techs.</Text>
